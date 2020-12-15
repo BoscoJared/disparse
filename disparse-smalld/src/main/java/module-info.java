2dd45c077c771260301314a09ac778cb078b0e89
Princess Lana <ianagbip1oti@gmail.com>
2020-12-15 16:00:33 +1100

    Update SmallD version (#37)
    * Update SmallD version

--- disparse-smalld/src/main/java/module-info.java
+++ disparse-smalld/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module disparse.smalld {
   requires com.google.gson;
   requires disparse.core;
   requires org.slf4j;
-  requires smalld;
+  requires com.github.princesslana.smalld;
 
   exports disparse.discord.smalld;
   exports disparse.discord.smalld.guilds;
